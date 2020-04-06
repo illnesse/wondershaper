e5ad7d6c726bc9b2a0dd7c332cab0029b1c34d30
sbrendel <illnesse@gmail.com>
2020-04-06 18:09:59 +0200

    testing

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 rm -rf wondershaper/
+echo -e "\n installing ---------------------------------------------------\n"
 git clone https://github.com/illnesse/wondershaper.git
-/wondershaper/wondershaper -a venet0 -u 1000
+echo -e "\n clearing conf ------------------------------------------------\n"
+/wondershaper/wondershaper -c -a venet0
+echo -e "\n setting up ---------------------------------------------------\n"
+/wondershaper/wondershaper -a venet0 -u 100000
+echo -e "\n info ---------------------------------------------------------\n"
+/wondershaper/wondershaper -s -a venet0
